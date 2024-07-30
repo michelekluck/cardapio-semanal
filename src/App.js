6a1f6368db4167b882875b08f22baef8a21fb22d
Michele Kluck <michelenamur27@gmail.com>
2024-07-30 11:24:00 -0300

    add logo e primeiro componente

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './assets/logo_mommys.png' 
+import DiaSemana from './DiaSemana.jsx'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+     <div className='header'>
+      <header className='app-header'>
+        <img src={logo} alt="Logo" className='logo' />
       </header>
+     </div>
+     <DiaSemana />
     </div>
   );
 }
---
+++ src/DiaSemana.jsx
@@ -0,0 +1,12 @@
+
+import './DiaSemana.css'
+
+function DiaSemana() {
+    return (
+        <div className="caixa-fundo">
+            <h2 className="more-sugar-font">Segunda-Feira</h2>
+        </div>
+    )
+}
+
+export default DiaSemana;
